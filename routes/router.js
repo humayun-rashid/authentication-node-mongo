9730e17995fb598203c67123e65aaafe25e8fa7f
humayun.rashid@dreambroker.fi <humayun.rashid@dreambroker.fi>
2020-02-26 16:07:08 +0200

    Updated

--- routes/router.js
+++ routes/router.js
@@ -13,7 +13,7 @@ router.get('/',async function(req,res){
     }
 })
 
-router.post('/', async function(req,res){
+router.post('/register', async function(req,res){
     const salt = await bcrypt.genSalt()
     const hashedPassword = await bcrypt.hash(req.body.password,salt)
     const user = new User({ 
@@ -30,6 +30,39 @@ router.post('/', async function(req,res){
     }
     
 })
+
+router.post('/login',userByEmail, async function(req,res){
+    const user = res.user
+    try{
+        if (await bcrypt.compare(req.body.password,user.password)){
+            res.send('User Authentication is successful')
+        } else {
+            res.send('User authentication is failed')
+        }
+
+
+    } catch(err){
+        res.status(400).json({message:err.message})
+    }
+
+})
+
+async function userByEmail (req,res,next) {
+    let user
+    try{
+        user = await User.findOne({email: req.body.email})
+        if (user == null) {
+            res.status(404).send("User with this email not found")
+        }
+
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
+    res.user = user
+    next()
+}
+
+
 
 async function userByID (req,res, next){
     let user
